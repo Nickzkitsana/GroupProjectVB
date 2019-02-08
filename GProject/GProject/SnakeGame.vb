5a2f3f055652552a8eb3f45201d9ceb16893ce29
kitsana phomgarm <nick7266@hotmail.com>
2019-02-09 03:01:29 +0700

    แก้ลูปไม่ได้

--- GProject/GProject/SnakeGame.vb
+++ GProject/GProject/SnakeGame.vb
@@ -60,7 +60,6 @@ Public Class SnakeGame
                 DrawGraphics()
                 If once = 0 Then
                     MessageBox.Show("GameOver")
-
                     Me.Close()
                     Dim name As String
                     Dim message = "Enter your name"
@@ -76,6 +75,7 @@ Public Class SnakeGame
                             Exit Sub
                         End If
                     End While
+
                     MessageBox.Show("GAMEOVER" & vbNewLine & "Score : " & score & vbNewLine & "Name : " & name, "GameOver", MessageBoxButtons.OK, MessageBoxIcon.Information)
                     Dim frm = MessageBox.Show("You need to insert data to database ?", "Submit", MessageBoxButtons.OKCancel, MessageBoxIcon.Question)
                     If frm = DialogResult.OK Then
